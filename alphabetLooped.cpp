7e1c24ecfaa48cafbd245e85b936cf523d40a0c6
Sean Staz <seanstaz52@gmail.com>
2014-12-21 19:16:37 -0600

    Create alphabetLooped.cpp
    Sean Stasny
    CSC 2134
    Alphabet
    Program Alphabet asks the user to input up to ten characters. The program
    then converts the first ten characters of the input to their corresponding 
    International Civil Aviation Organization alphabet words.

---
+++ alphabetLooped.cpp
@@ -0,0 +1,176 @@
+/*
+Sean Stasny
+CSC 2134
+Alphabet
+Program Alphabet asks the user to input up to ten characters. The program
+then converts the first ten characters of the input to their corresponding 
+International Civil Aviation Organization alphabet words.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+void function (char userinput) // Declares void function
+{
+ char a;			// Defines char
+ a = userinput;		// Accociates string with the user input
+
+    if (a == 'A' || a == 'a')       	// If/Else for using ICAO alphabet
+  {
+    cout << "Alpha " << endl;
+  }
+    else
+   if (a == 'B' || a == 'b')
+  {
+    cout << "Bravo " << endl;
+  }
+	else
+   if (a == 'C' || a == 'c')
+  {
+    cout << "Charlie " << endl;
+  }
+  else
+   if (a == 'D' || a == 'd')
+  {
+    cout << "Delta " << endl;
+  }
+  else
+   if (a == 'E' || a == 'e')
+  {
+    cout << "Echo " << endl;
+  }
+  else
+   if (a == 'F' || a == 'f')
+  {
+    cout << "Foxtrot " << endl;
+  }
+  else
+   if (a == 'G' || a == 'g')
+  {
+    cout << "Golf " << endl;
+  }
+  else
+   if (a == 'H' || a == 'h')
+  {
+    cout << "Hotel " << endl;
+  }
+  else
+   if (a == 'I' || a == 'i')
+  {
+    cout << "India " << endl;
+  }
+  else
+   if (a == 'J' || a == 'j')
+  {
+    cout << "Juliet " << endl;
+  }
+  else
+   if (a == 'K' || a == 'k')
+  {
+    cout << "Kilo " << endl;
+  }
+  else
+   if (a == 'L' || a == 'l')
+  {
+    cout << "Lima " << endl;
+  }
+  else
+   if (a == 'M' || a == 'm')
+  {
+    cout << "Mike " << endl;
+  }
+  else
+   if (a == 'N' || a == 'n')
+  {
+    cout << "November " << endl;
+  }
+  else
+   if (a == 'O' || a == 'o')
+  {
+    cout << "Oscar " << endl;
+  }
+  else
+   if (a == 'P' || a == 'p')
+  {
+    cout << "Papa " << endl;
+  }
+  else
+   if (a == 'Q' || a == 'q')
+  {
+    cout << "Quebec " << endl;
+  }
+  else
+   if (a == 'R' || a == 'r')
+  {
+    cout << "Romeo " << endl;
+  }
+  else
+   if (a == 'S' || a == 's')
+  {
+    cout << "Sierra " << endl;
+  }
+  else
+   if (a == 'T' || a == 't')
+  {
+    cout << "Tango " << endl;
+  }
+  else
+   if (a == 'U' || a == 'u')
+  {
+    cout << "Uniform " << endl;
+  }
+  else
+   if (a == 'V' || a == 'v')
+  {
+    cout << "Victor " << endl;
+  }
+  else
+   if (a == 'W' || a == 'w')
+  {
+    cout << "Whiskey " << endl;
+  }
+  else
+   if (a == 'X' || a == 'x')
+  {
+    cout << "X-ray " << endl;
+  }
+  else
+   if (a == 'Y' || a == 'y')
+  {
+    cout << "Yankee " << endl;
+  }
+  else
+   if (a == 'Z' || a == 'z')
+  {
+    cout << "Zulu " << endl;
+  }
+  else
+   if (a == ' ')
+  {
+  	cout << " " << endl;
+  } 
+  else
+  	cout << endl;
+}
+  	
+int main ()
+{
+	char a; 	   // Defines userinput
+	int count = 1; // Defines a count for the loop
+		
+		cout << "Enter ten characters to be translated through the" << endl; 			// An instruction statement
+		cout << "International Civil Aviation Organization alphabet words. " << endl;
+		cout << "(Blanks count as characters. Symbols are not recognized.)" << endl;
+		while (count <= 10) // Limits the count to 10
+		{
+			cin.get(a);		// Gets the input
+			function (a);	// Puts input into the fuction
+			count++;		// Adds 1 to the count
+		}
+
+
+  return 0;
+
+}
